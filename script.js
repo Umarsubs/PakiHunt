cc0eb97d1377a14942df3b8d86f63b7a2a85f8c2
Umar Farooq <134133924+Umarsubs@users.noreply.github.com>
2024-05-27 12:48:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
             const serverInfoHtml = `
                 <div class="server-details">
                     <div><span>Host:</span> ${data.host}</div>
-                    <div><span>Port:</span> ${data.port}</div>
                     <div><span>Name:</span> ${data.data.name}</div>
                     <div><span>Map:</span> ${data.data.map}</div>
-                    <div><span>Password Protected:</span> ${data.data.password}</div>
-                    <div><span>Max Players:</span> ${data.data.maxplayers}</div>
-                    <div><span>Owners:</span> ${data.data.raw.Owners}</div>
-                    <div><span>Email:</span> <a href="${data.data.raw.Email}">${data.data.raw.Email}</a></div>
-                    <div><span>Location:</span> ${data.data.raw.Location}</div>
-                    <div><span>Discord:</span> ${data.data.raw.Discord}</div>
-                </div>
-                <div class="raw-details">
-                    <h2>Raw Details:</h2>
-                    <pre>${JSON.stringify(data.data.raw, null, 2)}</pre>
+                    <div><span>Players:</span>  ${data.data.players} / ${data.data.maxplayers}</div>
                 </div>
             `;
 
